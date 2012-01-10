cc8ce9bd4107667bb8cbb2369ac0694631e8a79b
unknown <James@.(none)>
2012-01-10 13:35:18 +0000

    commit for deploy to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
 	:path => ":rails_root/public/images/userimages/:id/:style/:basename.:extension"
 	
 	validates_attachment_presence :photo
-	validates_attachment_size :photo, :less_than => 5.megabytes
+	validates_attachment_size :photo, :less_than => 1.megabytes
 	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
     
 	def create_digest(message)
