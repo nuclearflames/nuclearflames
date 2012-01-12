0f7c4f7c707170768d950c18c12a9ebf1068d094
unknown <James@.(none)>
2012-01-12 22:07:12 +0000

    semanticUpdates

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -9,6 +9,6 @@ has_attached_file :file,
   :path => ":rails_root/public/documents/userfiles/:id/:basename.:extension"
 
 validates_attachment_presence :file
-validates_attachment_size :file, :less_than => 3.megabytes
+validates_attachment_size :file, :less_than => 0.5.megabytes
 #validates_attachment_content_type :file, :content_type => ['image/docx', 'image/jpeg']
 end
