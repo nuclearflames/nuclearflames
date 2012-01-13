c53679397a86a1513b467e21b0f92a5a441fb483
unknown <James@.(none)>
2012-01-13 00:55:07 +0000

    messages

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-ActionController::Base.session_options[:session_secure] = true
+#ActionController::Base.session_options[:session_secure] = true
 # Initialize the rails application
 Nuclearflames::Application.initialize!
 
