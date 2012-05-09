2dbe39f013ee7f905b8eedb35fc5f37b650744a1
James Grant <jamesgrant1993@yahoo.com>
2012-05-09 01:12:40 +0100

    herokuFix1

---
+++ public/assets/application.js
@@ -0,0 +1,3 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+$(function(){$("#timeline_date").datepicker({dateFormat:"yy-mm-dd"})});
