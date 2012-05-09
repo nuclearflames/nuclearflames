ad41e4e06f981cb1cfb975950870100b9bfc3104
James Grant <jamesgrant1993@yahoo.com>
2012-05-09 01:28:01 +0100

    herokuFix1.03

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Nuclearflames::Application.configure do
 	config.assets.compress = true
 
 	# Don't fallback to assets pipeline
-	config.assets.compile = false
+	config.assets.compile = true
 
 	# Generate digests for assets URLs
 	config.assets.digest = true
